38ffd34ccbfa9abf322455efe86da14721dc8fe5
SergeyStryzhakov <sergey.stryzhakov@gmail.com>
2021-10-12 23:11:54 +0300

    Tests passed!

--- src/main/java/ua/edu/sumdu/j2se/stryzhakov/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/stryzhakov/tasks/ArrayTaskList.java
@@ -3,27 +3,23 @@ package ua.edu.sumdu.j2se.stryzhakov.tasks;
 import java.util.Arrays;
 
 public class ArrayTaskList {
-    private Task[] tasks = new Task[5];
-    private int indexArray = 0;
+    private Task[] tasks = new Task[0];
 
-    public void addTask(Task task) {
-        if (indexArray == tasks.length) {
-            tasks = Arrays.copyOf(tasks, tasks.length + 5);
-        }
-        tasks[indexArray] = task;
-        indexArray++;
+
+    public void add(Task task) {
+        tasks = Arrays.copyOf(tasks, tasks.length + 1);
+        tasks[tasks.length - 1] = task;
     }
 
-    public boolean removeTask(Task task) {
+    public boolean remove(Task task) {
         if (tasks.length == 0) return false;
         for (int i = 0; i < tasks.length; i++) {
             if (tasks[i].equals(task)) {
                 tasks[i] = null;
-                indexArray--;
-                Task[] temp = new Task[tasks.length - i];
-                System.arraycopy(tasks, i, temp, 0, tasks.length - i);
-                System.arraycopy(temp, 1, tasks, i, temp.length - 1);
-                System.out.println(Arrays.toString(tasks));
+                Task[] temp = new Task[tasks.length - 1];
+                System.arraycopy(tasks, 0, temp, 0, i);
+                System.arraycopy(tasks, i + 1, temp, i, tasks.length - i - 1);
+                tasks = temp;
                 return true;
             }
         }
@@ -43,7 +39,21 @@ public class ArrayTaskList {
 
     public ArrayTaskList incoming(int from, int to) {
         ArrayTaskList taskList = new ArrayTaskList();
-
+        for (Task task : tasks) {
+            if (task == null || !task.isActive()) continue;
+            if (!task.isRepeated() &&
+                    task.getTime() > from &&
+                    task.getTime() < to) {
+                taskList.add(task);
+            }
+            if (task.isRepeated() &&
+                    task.nextTimeAfter(from) != -1 &&
+                    task.nextTimeAfter(from) < to) {
+                taskList.add(task);
+            }
+        }
+        System.out.println("Incoming array: ");
+        System.out.println(Arrays.toString(taskList.tasks));
         return taskList;
     }
 }
--- src/main/java/ua/edu/sumdu/j2se/stryzhakov/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/stryzhakov/tasks/Main.java
@@ -6,17 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayTaskList list = new ArrayTaskList();
-        list.addTask(new Task("Test1", 15));
-        list.addTask(new Task("Test2", 10, 24, 2));
-        list.addTask(new Task("Test3", 18));
-        list.addTask(new Task("Test4", 8, 22, 3));
+        list.add(new Task("Test1", 15));
+        list.add(new Task("Test2", 10, 24, 2));
+        list.add(new Task("Test3", 18));
+        list.add(new Task("Test4", 8, 22, 3));
         Task test5 = new Task("Test5", 84);
+        test5.setActive(true);
         Task test6 = new Task("Test6", 98);
-        list.addTask(test5);
+        test6.setActive(true);
+        list.add(test5);
         System.out.println(list.size());
 
-        list.addTask(test6);
+        list.add(test6);
         System.out.println(list.size());
-        list.removeTask(test5);
+        list.remove(test5);
+        list.remove(test6);
+        list.remove(list.getTask(0));
+        list.incoming(80, 100);
     }
 }
